a54418719b8d69b94488c5bfe9c4f063c3370dce
Esanpons <ecs200@hotmail.com>
2023-09-17 12:50:29 +0200

    cambios en el "ViewVideo"

--- ViewVideo/src/controladdin/ViewVideo/ViewVideo.ControlAddin.al
+++ ViewVideo/src/controladdin/ViewVideo/ViewVideo.ControlAddin.al
@@ -7,7 +7,6 @@ controladdin "ViewVideo"
     HorizontalStretch = true;
     HorizontalShrink = true;
 
-    StyleSheets = 'ViewVideo/src/controlAddIn/ViewVideo/css/Style.css';
     Scripts = 'ViewVideo/src/controlAddIn/ViewVideo/js/Script.js',
                 'https://code.jquery.com/jquery-3.6.0.min.js';
     StartupScript = 'ViewVideo/src/controlAddIn/ViewVideo/js/Startup.js';
--- ViewVideo/src/controladdin/ViewVideo/js/Script.js
+++ ViewVideo/src/controladdin/ViewVideo/js/Script.js
@@ -14,24 +14,4 @@ function CargarVideo() {
             );
         }
     );
-    /*
-    $.get(
-        Microsoft.Dynamics.NAV.GetImageResource("ViewVideo/src/controlAddIn/video/html/HtmlHeader.html"),
-        function (Externo1) {
-            Html = Externo1;
-            controlAddIn = $("#controlAddIn");
-
-            controlAddIn.empty();
-            controlAddIn.html(Html);
-            
-            var video = $("#video-local");
-            video.attr(
-                "src",
-                Microsoft.Dynamics.NAV.GetImageResource("ViewVideo/src/controlAddIn/ViewVideo/video/VideoEjemplo.mp4")
-            );
-
-            
-        }
-    );
-    */
 }
